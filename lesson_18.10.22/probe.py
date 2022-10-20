cbd70abba7997b9a892cb6b1c7c8c612314a3cf3
arkey39 <arkey39@yandex.ru>
2022-10-20 20:29:10 +0300

    Just changes

--- lesson_18.10.22/probe.py
+++ lesson_18.10.22/probe.py
@@ -16,19 +16,8 @@
 #    pass
 
 # DRY – Dont repeat yourself
-#def greeting(parameter1):
-#    print('Привет,', parameter1)
-#hello_lst = ['Мир', 'Земля', 'Космос']
-#for elem in hello_lst:
-#    greeting(elem)
-
-
-
-def make_coffee(size, sugar_dose = 2): # "= 2" - базовое количество переменной. Функция будет работать, даже если не указать переменную.
-    if sugar_dose > 5:
-        return 'Слишком много сахара!'
-    else:
-        return f'Ваш кофе объёмом {size} мл с {sugar_dose} кусочками сахара готов!'
-# Результат функции нужно сохранят в переменную (при наличии return в функции)
-text = make_coffee(300)
-print(text)
+def greeting(parameter1):
+    print('Привет,', parameter1)
+hello_lst = ['Мир', 'Земля', 'Космос']
+for elem in hello_lst:
+    greeting(elem)
